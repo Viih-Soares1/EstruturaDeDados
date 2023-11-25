b15aafe5c301adba4fe8bb2ebf52812b38ea9641
Vinicius <viih.soares5030@gmail.com>
2023-11-25 17:36:57 -0300

    reposicao

---
+++ reposicao/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>  
+   
+    <form method="get" action="res.php">
+        <input type="text" name="nome">
+        <input type="email" name="email">
+        <input type="number" name="numero">
+        <input type="password" name="senha" require>
+        <input type="date" name="data">
+        <input type="month" name="mes">
+        <input type="tel" name="tel">
+        <input type="radio" name="radio">
+        <input type="search" name="pesquisa">
+        <input type="image" name="img" src="https://placehold.co/50x50">
+        <input type="color" name="favcolor">
+        <input type="file" name="arquivo">
+        <input type="submit" value="Enviar">
+    </form> 
+    
+</body>
+</html>
---
+++ reposicao/res.php
@@ -0,0 +1,9 @@
+<?php 
+    $nome = isset($_GET['nome']) ? $_GET['nome'] : 'vazio';
+    $email = isset($_GET['email']) ? $_GET['email'] : 'vazio';
+    $tel = isset($_GET['numero']) ? $_GET['numero'] : 'vazio';
+    $data = isset($_GET['data']) ? $_GET['data'] : 'vazio';
+    
+
+
+?>
